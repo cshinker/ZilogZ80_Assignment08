5a91dcc047fb167e10a1997fcce9709e35b4252e
Cam Shinker <shinkecj@mail.uc.edu>
2025-03-06 10:04:11 -0500

    Add project files.

---
+++ FunWithAIBenchmarkData/mainPackage/main.py
@@ -0,0 +1,66 @@
+# File Name: main.py
+# Student Name: Cam Shinker
+# email: shinkecj@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 3/27/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Create a data viz using this AI data
+
+# Brief Description of what this module does: Learning about how to work with different data types.
+# Citations: 
+
+# Anything else that's relevant:
+
+from readingLevelPackage.readingLevel import Reading_Level
+from utilitiesPackage.utilities import *
+from utilitiesPackage.CSV_Utilities import *
+from PDFPackage.PDFUtilities import *
+
+if __name__ == "__main__":
+
+    CSV_Processor = MMLU_CSV_Processor("dataPackage/MMLU/data/", ["management_test.csv"])
+    questions = CSV_Processor.read_data()
+    print(len(questions), "questions read")
+
+ 
+    # Start from here
+
+
+
+
+
+    #myPDF = PDFUtilities()
+    #myPDF.create_question_PDF("Management Test", "MMLU", questions)
+   
+    #print("The first question:")
+    #print(questions[0])
+    
+    text = convert_dictionaries_to_string(questions, ["prompt", "possible answers"])
+    #print("\ntext from dictionaries:", text[0:500])
+
+    #0. Append all the prompts into a big string - See utilities.convert_dictionaries_to_string()
+    
+    
+    #1. Perform reading level analysis on the big string and print the results to the console.
+    Reading_Level.compute_readability_indices("MMLU", text)
+
+  
+    questions_written = write_questions_to_text_files("MMLU", questions)
+    print(questions_written, "questions written to the file.")
+    
+    """
+    # This code is commented out
+    #Reading_Level.test01()
+
+    text = "This is a sentence that we can use to test the reading level computations. "
+    reading_level_indices = Reading_Level.compute_readability_indices("Dummy Benchmark", text)
+    for key in reading_level_indices.keys():
+        print(key, ":", reading_level_indices[key])
+            
+    # A test with text at a higher reading level
+    text = "Birds, employing a combination of aerodynamic principles and specialized anatomical adaptations, achieve flight through the generation of lift, which counteracts the force of gravity."
+    reading_level_indices = Reading_Level.compute_readability_indices("Dummy Benchmark", text)
+    for key in reading_level_indices.keys():
+        print(key, ":", reading_level_indices[key])
+    """
